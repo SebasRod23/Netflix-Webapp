ef87f4887b6e28d66e7bf56b6c51e8ab97d08c3c
GinaJame <gina10111@hotmail.com>
2021-05-21 21:49:37 -0500

    Bug fixed

--- netflix-webapp/src/components/Chart.tsx
+++ netflix-webapp/src/components/Chart.tsx
@@ -6,43 +6,28 @@ import axios, { AxiosResponse } from 'axios';
 const chartCss = css({
     width: "100%",
     display: "flex",
+    minHeight: '48.7vh',
     flexDirection:"column",
     alignContent: "center", 
     margin:"10px",
 
 });
 const chartc = css({
-alignSelf:"center"
+    alignSelf:"center"
 }
 );
-const chartConfig = {
-    series: [100],
-    chart: {
-        id: 'mychart',
-        width: 500,
-        type: 'pie',
-    },
-    labels: ['all'],
-    legend: {
-        show: false
-    },
-    responsive: [{
-        breakpoint: 480,
-        options: {
-        chart: {
-            width: 250
-        }
-        }
-    }],
-};
+
 interface response{
     series:number[],
     labels:any[]
 }
+interface StatisticsProps{
+    typeProps:string
+}
 let chart:any=false
-const Chart: React.FC = () => {
-    const [label,setLabel]=useState({})
-    const [type,setType]=useState("")
+
+const Chart: React.FC<StatisticsProps> = ({ typeProps }) => {
+    const [type,setType]=useState(typeProps)
     const getTodos = async (): Promise<AxiosResponse<any>> => {
         try {
             const todos: AxiosResponse<any> = await axios.get(
@@ -51,134 +36,144 @@ const Chart: React.FC = () => {
             console.log(todos)
             let chartTodo:response = await todos.data
             console.log(chartTodo.labels)
-            ApexCharts.exec('mychart', 'updateOptions', {
-                series: chartTodo.series,
-                chart: {
-                    id: 'mychart',
-                    width: '500px',
-                    type: 'pie',
-                },
-                labels: chartTodo.labels,
-                legend: {
-                    show: false
-                },
-                responsive: [{
-                    breakpoint: 480,
-                    options: {
+                chart =  new ApexCharts(document.querySelector("#chart"),  {
+                    series: chartTodo.series,
                     chart: {
-                        width: 250
-                    }
-                    }
-            }]});
+                        id: 'mychart',
+                        width: '500px',
+                        type: 'pie',
+                    },
+                    labels: chartTodo.labels,
+                    legend: {
+                        show: false
+                    },
+                    responsive: [{
+                        breakpoint: 480,
+                        options: {
+                        chart: {
+                            width: 250
+                        }
+                        }
+                }]})
+            chart.render();
             return todos;
         } catch (error) {
             throw new Error(error);
         }
     };
+    
     const getCountry= async (): Promise<AxiosResponse<any>> => {
         try {
             const dataCountry: AxiosResponse<any> = await axios.get(
                 'http://localhost:3010/statistics/country',
             );
             let chartTodo:response=await dataCountry.data
-            await ApexCharts.exec('mychart','updateOptions',{
-                series: [{
-                    data: chartTodo.series
-                }],
-                chart: {
-                    type: 'bar',
-                    width:'600px'
-                },
-                plotOptions: {
-                    bar: {
-                        columnWidth: '50%',
-                        distributed: true,
+                chart =  new ApexCharts(document.querySelector("#chart"), {
+                    series: [{
+                        data: chartTodo.series
+                    }],
+                    chart: {
+                        type: 'bar',
+                        width:'600px',
+                        id:'mychart'
+                    },
+                    plotOptions: {
+                        bar: {
+                            columnWidth: '50%',
+                            distributed: true,
+                        }
+                    },
+                    dataLabels: {
+                        enabled: false
+                    },
+                    legend: {
+                        show: false
+                    },
+                    xaxis: {
+                        categories: chartTodo.labels
+                    },tooltip: {
+                        theme: 'dark',
+                        y: {
+                            title: {
+                                formatter: function () {
+                                  return 'movies'
+                                }
+                            }
+                        },
                     }
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                legend: {
-                    show: false
-                },
-                xaxis: {
-                    categories: chartTodo.labels
-                }
-            })
+                })
+           
+            chart.render();
             return dataCountry;
         } catch (error) {
             throw new Error(error);
         }
     };
-    const getPerCountry= async (country:string): Promise<AxiosResponse<any>> => {
-        try {
-            const numCountry: AxiosResponse<any> = await axios.get(
-                'http://localhost:3010/statistics/country/'+country,
-            );
-            setLabel(numCountry)
-            return numCountry;
-        } catch (error) {
-            throw new Error(error);
-        }
-    };
+
     const getYear= async (): Promise<AxiosResponse<any>> => {
         try {
             const dataYear: AxiosResponse<any> = await axios.get(
                 'http://localhost:3010/statistics/year',
             );
             let chartTodo:response=await dataYear.data
-            await ApexCharts.exec('mychart','updateOptions',{
-                series: [{
-                    data: chartTodo.series
-                }],
-                chart: {
-                    type: 'bar',
-                    width:'700px'
-                },
-                plotOptions: {
-                    bar: {
-                        columnWidth: '50%',
-                        distributed: true,
+                chart =  new ApexCharts(document.querySelector("#chart"), {
+                    series: [{
+                        data: chartTodo.series
+                    }],
+                    chart: {
+                        type: 'bar',
+                        width:'700px',
+                        id:'mychart'
+                    },
+                    plotOptions: {
+                        bar: {
+                            columnWidth: '50%',
+                            distributed: true,
+                        }
+                    },
+                    dataLabels: {
+                        enabled: false
+                    },
+                    legend: {
+                        show: false
+                    },
+                    xaxis: {
+                        categories: chartTodo.labels
+                    },tooltip: {
+                        theme: 'dark',
+                        y: {
+                            title: {
+                                formatter: function () {
+                                  return 'TV shows'
+                                }
+                            }
+                        },
+                        
                     }
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                legend: {
-                    show: false
-                },
-                xaxis: {
-                    categories: chartTodo.labels
-                }
-            })
+                })  
+            chart.render();
             return dataYear;
         } catch (error) {
             throw new Error(error);
         }
     };
-    const getPerYear= async (yearNum:number): Promise<AxiosResponse<any>> => {
-        try {
-            const year: AxiosResponse<any> = await axios.get(
-                'http://localhost:3010/statistics/year/'+yearNum,
-            );
-            setLabel(year)
-            return year;
-        } catch (error) {
-            throw new Error(error);
-        }
-    };
+    
     useEffect(() => {
-        if(!chart){
-            chart =  new ApexCharts(document.querySelector("#chart"), chartConfig)  
-            chart.render();
+        console.log("entre")
+        const fetchData=()=>{
+            if(chart){  
+                chart.destroy();
+            }
+            console.log("wiii")
+            if(type==="country"){
+                getCountry()
+            }else if(type==="year"){
+                getYear()
+            } else{
+                getTodos()
+            }
         }
-        if(type==="country"){
-            getCountry()
-        }else if(type==="year"){
-            getYear()
-        } else{
-            getTodos()
-        }
+        fetchData()
     },[type]);
 
 
