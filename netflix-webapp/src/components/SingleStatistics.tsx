56bfff140c3133a164df0089c6cc515a79832db5
GinaJame <gina10111@hotmail.com>
2021-05-21 21:49:37 -0500

    Bug fixed

---
+++ netflix-webapp/src/components/SingleStatistics.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import axios, { AxiosResponse } from 'axios';
+/** @jsxImportSource @emotion/react */ import { css } from "@emotion/react";
+
+const chartCss = css({
+    width: "100%",
+    minHeight: '48.7vh',
+    display: "flex",
+    flexDirection:"column",
+    alignContent: "center", 
+    justifyContent:"center",
+    margin:"10px",
+    "h2":{
+        alignSelf:"center",
+        marginBottom:"20px"
+    },
+    "h3":{
+        alignSelf:"center",
+        margin:"20px",
+        color:'#781c16',
+        fontWeight: 500,
+    }
+});
+
+interface StatisticsProps{
+    type:string,
+    input:string
+}
+const SingleStatistics: React.FC<StatisticsProps> = ({ type,input }) => {
+    const [label,setLabel]=useState(type)
+    const [numLabel,setNumLabel]=useState()
+    
+    const getPerCountry= async (country:string): Promise<AxiosResponse<any>> => {
+        try {
+            const numCountry: AxiosResponse<any> = await axios.get(
+                'http://localhost:3010/statistics/country/'+country,
+            );
+            setLabel(country)
+            setNumLabel(numCountry.data)
+            console.log(numCountry)
+            return numCountry;
+        } catch (error) {
+            throw new Error(error);
+        }
+    };
+
+    const getPerYear= async (yearNum:number): Promise<AxiosResponse<any>> => {
+        try {
+            const year: AxiosResponse<any> = await axios.get(
+                'http://localhost:3010/statistics/year/'+yearNum,
+            );
+            setLabel(yearNum.toString())
+            setNumLabel(year.data)
+            return year;
+        } catch (error) {
+            throw new Error(error);
+        }
+    };
+    useEffect(() => {
+        if(type==="country"){
+            getPerCountry(input)
+        }else{
+            let yearNum:number=+input
+            getPerYear(yearNum)
+        } 
+    },[type]);
+
+    return (
+        <div css={chartCss}>
+            <h2>{label}</h2>
+            <h3>{numLabel}</h3>
+        </div>
+        
+    );
+};
+  
+export default SingleStatistics;
--- netflix-webapp/src/components/StatisticsView.tsx
+++ netflix-webapp/src/components/StatisticsView.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Chart from "./Chart";
+import SingleStatistics from "./SingleStatistics";
 /** @jsxImportSource @emotion/react */ import { css } from "@emotion/react";
 
 const StatisticsStyle = css({
@@ -7,13 +8,26 @@ const StatisticsStyle = css({
   fontSize: "2rem",
   display: "flex",
   flexDirection: "column",
+  "h1":{
+    fontSize: "3rem",
+    alignSelf:"center"
+}
 });
-
-const StatisticsView: React.FC = () => {
+interface StatisticsProps{
+  type:string,
+  input:string
+}
+//const StatisticsView: React.FC<StatisticsProps> = ({ type,input }) => {
+const StatisticsView: React.FC= () => {
+  const [input, setInput]=useState('')
+  //let renderedComp = input === '' ? <Chart typeProps={type}/> : <SingleStatistics input={input} type={type}/>;
+  let renderedComp = input === '' ? <Chart typeProps='all'/> : <SingleStatistics input='Brazil' type='country'/>;
   return (
     <div css={StatisticsStyle}>
+      {//<h1>Statistics {type}</h1>
+      }
       <h1>Statistics</h1>
-      <Chart />
+      {renderedComp}
     </div>
   );
 };
